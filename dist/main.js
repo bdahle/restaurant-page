edccaf97c677159b4882d3059c0fb8594bcb3eec
Bjørn Harbo Dahle <bdahle@hey.com>
2021-07-02 11:43:15 +0200

    create content with js

--- dist/main.js
+++ dist/main.js
@@ -6,30 +6,27 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-/******/ (() => {
-  // webpackBootstrap
-  /******/ var __webpack_modules__ = {
-    /***/ "./src/index.js":
-      /*!**********************!*\
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-      /***/ () => {
-        eval(
-          'console.log("test");\n\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?'
-        );
+/***/ (() => {
 
-        /***/
-      },
+eval("const content = document.getElementById(\"content\");\n\nconst headline = document.createElement(\"h1\");\nheadline.innerHTML = \"Ristorante Fantastico\";\n\nconst introduction = document.createElement(\"p\");\nintroduction.innerHTML =\n  \"Hos Ristorante Fantastico lager vi autentisk italiensk mat med en moderne vri.\";\n\nconst image = document.createElement(\"img\");\nimage.src = \"restaurant.jpg\";\n\ncontent.appendChild(headline);\ncontent.appendChild(introduction);\ncontent.appendChild(image);\n\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
 
-    /******/
-  };
-  /************************************************************************/
-  /******/
-  /******/ // startup
-  /******/ // Load entry module and return exports
-  /******/ // This entry module can't be inlined because the eval devtool is used.
-  /******/ var __webpack_exports__ = {};
-  /******/ __webpack_modules__["./src/index.js"]();
-  /******/
-  /******/
-})();
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
