b06753c199caa6719babfcd1632cb5fe6a122280
Bjørn Harbo Dahle <bdahle@hey.com>
2021-07-05 11:10:48 +0200

    cleanup commented code

--- src/homeComponent.js
+++ src/homeComponent.js
@@ -1,8 +1,6 @@
 import Restaurant from "./images/restaurant.jpg";
 
 function homeComponent() {
-  // const content = document.getElementById("content");
-
   const tab = document.createElement("div");
   const introduction = document.createElement("p");
   const image = document.createElement("img");
@@ -13,7 +11,6 @@ function homeComponent() {
 
   tab.appendChild(introduction);
   tab.appendChild(image);
-  // content.appendChild(tab);
 
   return tab;
 }
