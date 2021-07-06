ac710fc68f55202755085196492f47211266f1dc
Bjørn Harbo Dahle <bdahle@hey.com>
2021-07-06 10:03:10 +0200

    update contact info

--- src/contactComponent.js
+++ src/contactComponent.js
@@ -2,14 +2,13 @@ import Outside from "./images/outside.jpg";
 
 function contactComponent() {
   const tab = document.createElement("div");
-  const introduction = document.createElement("p");
+  const address = document.createElement("p");
   const image = document.createElement("img");
 
-  introduction.innerHTML = `Adresse:
-  Fantasticoveien 1`;
+  address.innerHTML = "Adresse: Fantasticoveien 1, Oslo";
   image.src = Outside;
 
-  tab.appendChild(introduction);
+  tab.appendChild(address);
   tab.appendChild(image);
 
   return tab;
